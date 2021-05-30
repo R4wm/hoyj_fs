beb3b3a825bd407cb7b8894f4dd780eddee19def
rmintz <raymond.mintz@verizondigitalmedia.com>
2021-05-30 15:47:43 -0700

    remove beta prefix

--- utils/map_mp3_files.sh
+++ utils/map_mp3_files.sh
@@ -4,7 +4,7 @@ mp3_dir="/var/www/html/helpersofyourjoy/media/mp3"
 redis_prefix="HOYJ::MP3::MAP::DUMP"
 for i in $( find "$mp3_dir" -iname "*.mp3" -print)
 do
-    http_path=$(echo "$i" | sed 's/\/var\/www\/html\/helpersofyourjoy/https:\/\/b.helpersofyourjoy.com/')
+    http_path=$(echo "$i" | sed 's/\/var\/www\/html\/helpersofyourjoy/https:\/\/helpersofyourjoy.com/')
     echo "http_path: $http_path"
     basefile=$(basename "$i")
     echo "$basefile"
