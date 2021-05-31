9ecac2c4c64dae5fddc5bffc59ea9aa1de7b3ab7
rmintz <raymond.mintz@verizondigitalmedia.com>
2021-05-31 13:05:29 -0700

    make suitable for cron job to update redis db

--- utils/map_mp3_files.sh
+++ utils/map_mp3_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mp3_dir="/var/www/html/helpersofyourjoy/media/mp3"
-redis_prefix="HOYJ::MP3::MAP::DUMP"
+redis_prefix="HOYJ::MP3::MAP::DUMP::INPROGRESS"
 for i in $( find "$mp3_dir" -iname "*.mp3" -print)
 do
     http_path=$(echo "$i" | sed 's/\/var\/www\/html\/helpersofyourjoy/https:\/\/helpersofyourjoy.com/')
@@ -10,3 +10,5 @@ do
     echo "$basefile"
     redis-cli SADD "$redis_prefix" "$http_path"
 done
+
+redis-cli RENAME "$redis_prefix" "HOYJ::MP3::MAP::DUMP"
