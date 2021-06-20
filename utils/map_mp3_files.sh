37d16abb8f0e0ab87d692f5907617b97727ca69c
r4wm <raymondmintz11@gmail.com>
2021-06-20 21:16:50 +0000

    search for mp4 files

--- utils/map_mp3_files.sh
+++ utils/map_mp3_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mp3_dir="/var/www/html/helpersofyourjoy/media/mp3"
+mp3_dir="/var/www/html/helpersofyourjoy/media"
 redis_prefix="HOYJ::MP3::MAP::DUMP::INPROGRESS"
-for i in $( find "$mp3_dir" -iname "*.mp3" -print)
+for i in $( find "$mp3_dir" \( -iname "*.mp3" -o -iname "*.mp4" \) -print)
 do
     http_path=$(echo "$i" | sed 's/\/var\/www\/html\/helpersofyourjoy/https:\/\/helpersofyourjoy.com/')
     echo "http_path: $http_path"
