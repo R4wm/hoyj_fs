e93bafdce984403de120db9ff8966df5ce20e1e6
r4wm <raymondmintz11@gmail.com>
2021-09-01 00:41:08 -0700

    cleanup

--- utils/map_media_files.py
+++ utils/map_media_files.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+# pull down media list from Linodes object storage
+# create urls for each file
+# push those urls to redis
+# api knows how to parse search query and find url for related search query
+
 import redis
 from pathlib import Path
+
 def populate_redis(a_media_file: str) -> None:
     base_url='us-southeast-1.linodeobjects.com'
     if not Path(a_media_file).is_file():
@@ -15,23 +21,16 @@ def populate_redis(a_media_file: str) -> None:
     redis_inprogress = 'HOYJ::MP3::MAP::INPROGRESS'
     redis_handler = redis.Redis()
     
-    count = 0
     for line in lines:
         complete_url = ''
-        count += 1
         if line == '\n':
-            print('>> line is empty')
             continue
         else:
-            print("ok line: {}".format(line))
             line = line.split()[-1]
             message_title = line.split('/')[-1]
-            print('this is message_title: {}'.format(message_title))
-            print('line again: {}'.format(line))
             url_prefix = line.split('/')[0]
-            print('url prefix: {}'.format(url_prefix))
             complete_url='https://{}.{}/{}'.format(url_prefix, base_url, message_title)
-        print('this is complete url: {}'.format(complete_url))
+
         redis_handler.sadd(redis_inprogress , complete_url)
 
     redis_handler.rename(redis_inprogress, redis_path)
