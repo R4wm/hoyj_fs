3b1c73f7d983ef38d43ac5230b4ba19a150b15d2
r4wm <raymondmintz11@gmail.com>
2022-05-15 17:49:53 -0700

    fixing stuff for directory

--- api/server.go
+++ api/server.go
@@ -8,13 +8,56 @@ import (
 	"sort"
 	"strings"
 	"text/template"
+	"time"
 
 	"github.com/go-redis/redis"
+	"github.com/gorilla/mux"
 )
 
 var (
 	redisSetName = "HOYJ::MP3::MAP::DUMP"
 )
+
+const chapterTemplate = `
+<html>
+<title>{{.Name}}</title>
+<style>
+.btn-group button {
+  background-color: gold; /* Green background */
+  border: 1px solid green; /* Green border */
+  color: black;
+  padding: 10px 24px; /* Some padding */
+  cursor: pointer; /* Pointer/hand icon */
+  float: center; /* Float the buttons side by side */
+}
+/* Clear floats (clearfix hack) */
+.btn-group:after {
+  content: "";
+  clear: both;
+  display: table;
+}
+.btn-group button:not(:last-child) {
+  border-right: none; /* Prevent double borders */
+}
+/* Add a background color on hover */
+.btn-group button:hover {
+  background-color: #3e8e41;
+}
+</style>
+  <body style="background-color:{{ .Color }};">
+    <h1><center><a href=https://helpersofyourjoy.com/media>{{ .Name }}</a></h1>
+  <body>
+    {{ range $index, $results := .Series }}
+    <p><b><left><a href={{ $results }}> {{ createLink $results }}</a>  </b></p>
+    {{ end }}
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <div class="w3-bar">
+    <div class="btn-group">
+    </div>
+  </body>
+</html>
+`
 
 const tpl = `
 <!DOCTYPE html>
@@ -27,6 +70,40 @@ const tpl = `
 		{{range .Items}}<p><a href="{{ . }}">{{ basenameMP3Files . }}</a></p>{{else}}<div><strong>no rows</strong></div>{{end}}
 	</body>
 </html>`
+
+const chapterButtonsTemplate = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      .block {
+      display: block;
+      width: 100%;
+      border: none;
+      background-color: #4CAF50;
+      color: white;
+      padding: 14px 28px;
+      font-size: 16px;
+      cursor: pointer;
+      text-align: center;
+      }
+      .block:hover {
+      background-color: #ddd;
+      color: black;
+      }
+    </style>
+    <title>{{ .Name }}</title>
+  </head>
+  <body style="background-color:{{ .Color }};">
+    <p><center><h1> {{ .Name }} </h1><center></p>
+    <button onclick="window.location.href='https://helpersofyourjoy.com';" class="w3-bar-item w3-button" style="width:33.3%">Return to helpersofyourjoy.com</button>
+    {{ range $index, $results := .Series }}
+    <p><button class="block" onclick="window.location.href = '{{ add "./directory/" $results }}'">{{ $results }}</button></p>
+    {{ end }}
+  </body>
+</html>
+`
 
 func sanitizeArgs(args string) []string {
 	parsedArgs := strings.Split(args, " ")
@@ -111,6 +188,37 @@ func mp3Search(w http.ResponseWriter, r *http.Request) {
 	check(err)
 }
 
+func listMedia(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("listing series")
+	options := redis.Options{}
+	redisCli := redis.NewClient(&options)
+
+	result := redisCli.Keys("HOYJ::MEDIA::*").Val()
+	sort.Strings(result)
+	seriesAvailable := []string{}
+	for _, seriesTopic := range result {
+		seriesTopic = strings.SplitAfterN(seriesTopic, "::", 3)[2]
+		seriesAvailable = append(seriesAvailable, seriesTopic)
+	}
+	w.WriteHeader(http.StatusOK)
+
+	funcs := template.FuncMap{"add": func(x, y string) string { return x + y }}
+	t, err := template.New("seriesList").Funcs(funcs).Parse(chapterButtonsTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	Listing := struct {
+		Name   string
+		Series []string
+		Color  string
+	}{"Series", seriesAvailable, "White"}
+
+	t.Execute(w, Listing)
+
+}
+
 func mediaInfo(w http.ResponseWriter, r *http.Request) {
 	// Get everything from redis
 	rdb := redis.NewClient(&redis.Options{
@@ -133,12 +241,59 @@ func mediaInfo(w http.ResponseWriter, r *http.Request) {
 	w.Write(haha)
 }
 
+func topicDirectory(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("running topicDirectory")
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+
+	options := redis.Options{}
+	redisCli := redis.NewClient(&options)
+	// get keys from redis for topic
+	vars["category"] = strings.ToUpper(vars["category"])
+	redisKey := "HOYJ::MEDIA::" + vars["category"]
+	fmt.Println("redisKey: ", redisKey)
+
+	redisResult := redisCli.SMembers(redisKey).Val()
+	sort.Strings(redisResult)
+	fmt.Println("this is redis result: ", redisResult)
+	// if not exists return 404
+
+	// create template function to strip title from url
+
+	// pass in the slice of urls
+	funcs := template.FuncMap{"createLink": func(b string) string {
+		breakit := strings.Split(b, "/")
+		return breakit[len(breakit)-1]
+	}}
+
+	t, err := template.New("seriesLinks").Funcs(funcs).Parse(chapterTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	Listing := struct {
+		Name   string
+		Series []string
+		Color  string
+	}{vars["category"], redisResult, "White"}
+
+	t.Execute(w, Listing)
+}
+
 // main: run the search engine
 func main() {
-	port := "127.0.0.1:8082"
-	http.HandleFunc("/mp3/search", mp3Search)
-	http.HandleFunc("/media/info", mediaInfo)
-	if err := http.ListenAndServe(port, nil); err != nil {
-		log.Fatal(err)
+	r := mux.NewRouter()
+	r.HandleFunc("/directory/{category}", topicDirectory)
+	r.HandleFunc("/directory", listMedia)
+	r.HandleFunc("/mp3/search", mp3Search)
+	r.HandleFunc("/media/info", mediaInfo)
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8082",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
+	fmt.Println("running server")
+	log.Fatal(srv.ListenAndServe())
 }
