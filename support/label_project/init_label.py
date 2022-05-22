08e4f1ede2ddb61518d2f26529e291c209b1ff38
r4wm <raymondmintz11@gmail.com>
2022-05-21 21:33:02 -0700

    saving prog

--- support/label_project/init_label.py
+++ support/label_project/init_label.py
@@ -42,6 +42,7 @@ with open("labeling_conf.json") as r:
 
 class Message:
     def __init__(self, filename=''):
+        self.originalref = filename
         filename = filename.lower()
         self.filename = filename
         self.speaker = ''
@@ -55,14 +56,14 @@ class Message:
         self.part = ''
         self.verseStart = ''
         self.verseEnd = ''
-        
+
     def setSpeaker(self):
         if self.speaker != '':
             return
         for i in speakers:
             if i in self.filename.lower():
                 self.speaker = i.replace('_', ' ')
-                
+
     def setTopic(self):
         if '/' not in self.filename:
             print("skipping, no slash found in filename")
@@ -96,7 +97,7 @@ class Message:
                 return
         # default to whatever is there
         self.topic = topic
-            
+
     def setDate(self):
         foundDate = re.match(r'(.*)(\d{8})', self.filename)
         if foundDate:
@@ -107,7 +108,6 @@ class Message:
     def setMD5Sum(self):
         for i in MD5TOFILE:
             checkAgainst = self.filename.split('/')[1]
-            # import pdb;pdb.set_trace()
             if i[1] == checkAgainst:
                 self.md5sum = i[0]
                 return
@@ -118,6 +118,12 @@ class Message:
         if foundChapter:
             if self.chapter == '':
                 self.chapter = int(foundChapter.group(3))
+                return
+        cornerCase1 = re.match(r'(.*-_)([a-z]+)(_ch\._)(\d{1,3})', checkAgainst)
+        if cornerCase1:
+            if self.chapter == '':
+                self.chapter = cornerCase1.group(4)
+
 
     def setBook(self):
         checkAgainst = self.filename.split('/')[1]
@@ -125,12 +131,27 @@ class Message:
         if foundBook:
             if self.book == '':
                 self.book = foundBook.group(2)
+                return
+        cornerCase1 = re.match(r'(.*-_)([a-z]+)(_ch\._)(\d{1,3})', checkAgainst)
+        if cornerCase1:
+            if self.book == '':
+                self.book = cornerCase1.group(2)
+                return
+        # last resorts
+        if self.book == '':
+            for i in ['2_corinthians', '1_corinthians', '1_timothy',
+                      '2_timothy', '1_samuel', '2_samuel', '1_kings',
+                      '2_kings', '1_chronicals', '2_chronicals',
+                      '1_thessalonians', '2_thessalonians', '1_peter', '2_peter',
+                      '1_john', '2_john', '3_john']:
+                if i in self.filename:
+                    self.book = i.replace('_', ' ')
+                    return
 
     def setVerses(self):
         checkAgainst = self.filename.split('/')[1]
         # luke_ch_15_v_11_to_32.mp3
         foundVerses = re.match(r'(.*)(_ch_\d{1,3})(_)(v_)(\d{1,3})(_\w{1,3}_)(\d{1,3})', checkAgainst)
-        # import pdb;pdb.set_trace()
         if foundVerses:
             if self.verseStart == '' and self.verseEnd == '':
                 self.verseStart = int(foundVerses.group(5))
@@ -142,6 +163,9 @@ class Message:
             if self.verseStart == '':
                 self.verseStart = int(foundVerse.group(5))
                 return
+        # cornerCase1 = re.match(r'(.*-_)([a-z]+)(_ch\._)(\d{1,3})', checkAgainst)
+        # if cornerCase1:
+        #     import pdb;pdb.set_trace()
 
     def setPart(self):
         checkAgainst = self.filename.split('/')[1]
@@ -149,6 +173,13 @@ class Message:
         if foundPart:
             if self.part == '':
                 self.part = int(foundPart.group(2))
+                return
+        cornerCase1 = re.match(r'(.*)(_pt_)(\d{1,3})([-_])', checkAgainst)
+        if cornerCase1:
+            if self.part == '':
+                self.part = int(cornerCase1.group(3))
+                return
+
 
         
 # function to identify year, return Y-M-D
@@ -233,8 +264,45 @@ def main():
     x.setVerses()
     x.setPart()
     print(x.__dict__)
+
+    # testing verses with "AND"
+    print("doing y")
+    y = Message(filename='2021-james-series/20210110-JAMES_SERIES-PART_49_JOHN_VERSTEGEN-JAMES_CH_3_V_17_AND_18-WISDOM_FROM_ABOVE.mp3')
+    y.setTopic()
+    y.setSpeaker()
+    y.setDate()
+    y.setMD5Sum()
+    y.setChapter()
+    y.setBook()
+    y.setVerses()
+    y.setPart()
+    print(y.__dict__)
+
+    # cornercase 1
+    print("doing z")
+    z = Message(filename='winter-conference-2018/20180217-SAT-0900-WINTER_CONFERENCE_2018-RICK_JORDAN_-_Psalm_Ch._16_Luke_Ch._24_V._5_and_6._The_Message_Of_An_Empty_Tomb.mp3')
+    z.setTopic()
+    z.setSpeaker()
+    z.setDate()
+    z.setMD5Sum()
+    z.setChapter()
+    z.setBook()
+    z.setVerses()
+    z.setPart()
+    print(z.__dict__)
     
 
-    
+    for mediaFile in MEDIALIST:
+        a = Message(filename=mediaFile)
+        a.setTopic()
+        a.setSpeaker()
+        a.setDate()
+        a.setMD5Sum()
+        a.setChapter()
+        a.setBook()
+        a.setVerses()
+        a.setPart()
+        print(json.dumps(a.__dict__, indent=4))
+        
 if __name__ == '__main__':
     main()
