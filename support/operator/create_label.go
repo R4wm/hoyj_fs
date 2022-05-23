84d77e3d7d6a7e685371410744111c02fdfaeb51
r4wm <raymondmintz11@gmail.com>
2022-05-23 00:06:12 -0700

    finished?

--- support/operator/create_label.go
+++ support/operator/create_label.go
@@ -16,6 +16,75 @@ import (
 	"time"
 )
 
+var orderedBooks = []string{
+	"GENESIS",
+	"EXODUS",
+	"LEVITICUS",
+	"NUMBERS",
+	"DEUTERONOMY",
+	"JOSHUA",
+	"JUDGES",
+	"RUTH",
+	"1SAMUEL",
+	"2SAMUEL",
+	"1KINGS",
+	"2KINGS",
+	"1CHRONICLES",
+	"2CHRONICLES",
+	"EZRA",
+	"NEHEMIAH",
+	"ESTHER",
+	"JOB",
+	"PSALMS",
+	"PROVERBS",
+	"ECCLESIASTES",
+	"SONG OF SOLOMON",
+	"ISAIAH",
+	"JEREMIAH",
+	"LAMENTATIONS",
+	"EZEKIEL",
+	"DANIEL",
+	"HOSEA",
+	"JOEL",
+	"AMOS",
+	"OBADIAH",
+	"JONAH",
+	"MICAH",
+	"NAHUM",
+	"HABAKKUK",
+	"ZEPHANIAH",
+	"HAGGAI",
+	"ZECHARIAH",
+	"MALACHI",
+	"MATTHEW",
+	"MARK",
+	"LUKE",
+	"JOHN",
+	"ACTS",
+	"ROMANS",
+	"1CORINTHIANS",
+	"2CORINTHIANS",
+	"GALATIANS",
+	"EPHESIANS",
+	"PHILIPPIANS",
+	"COLOSSIANS",
+	"1THESSALONIANS",
+	"2THESSALONIANS",
+	"1TIMOTHY",
+	"2TIMOTHY",
+	"TITUS",
+	"PHILEMON",
+	"HEBREWS",
+	"JAMES",
+	"1PETER",
+	"2PETER",
+	"1JOHN",
+	"2JOHN",
+	"3JOHN",
+	"JUDE",
+	"REVELATION",
+}
+
 // Message the json output for label
 type Message struct {
 	Originalref string `json:originalref`
@@ -95,10 +164,12 @@ func (m *Message) setFileName(existing *[]Message) {
 	fmt.Println("filepath: ")
 	s := bufio.NewScanner(os.Stdin)
 	s.Scan()
-	filepath := s.Text()
-	info, err := os.Stat(filepath)
+	m.Originalref = s.Text()
+	filepathbreak := strings.Split(m.Originalref, "/")
+	filepath := filepathbreak[len(filepathbreak)-1]
+	info, err := os.Stat(m.Originalref)
 	if os.IsNotExist(err) {
-		log.Fatalf("%s not exists", filepath)
+		log.Fatalf("%s not exists", m.Originalref)
 	}
 	if info.IsDir() {
 		log.Fatalf("%s is a directory not a file.", filepath)
@@ -119,14 +190,14 @@ func (m *Message) setFileName(existing *[]Message) {
 
 func (m *Message) setMD5Sum() {
 	log.Println("running setMD5Sum")
-	f, err := os.Open(m.FileName)
+	f, err := os.Open(m.Originalref)
 	if err != nil {
-		log.Fatal("Failed to read (want to md5sum) %s", m.FileName)
+		log.Fatalf("Failed to read (want to md5sum) %s", m.Originalref)
 	}
 	defer f.Close()
 	h := md5.New()
 	if _, err := io.Copy(h, f); err != nil {
-		log.Fatalf("Failed md5sum %s", m.FileName)
+		log.Fatalf("Failed md5sum %s", m.Originalref)
 	}
 	m.Md5Sum = fmt.Sprintf("%x", h.Sum(nil))
 	// check if the md5 already exists??
@@ -135,6 +206,7 @@ func (m *Message) setMD5Sum() {
 func (m *Message) setTopic(existing *[]Message) {
 	log.Println("running setTopic")
 	// Taking input from user
+	fmt.Println("topic: ")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	topicCandidate := scanner.Text()
@@ -238,16 +310,117 @@ func (m *Message) setDay() {
 	m.Day = holder
 }
 
+func (m *Message) setBook() {
+	log.Println("running setBook")
+	// Taking input from user
+	fmt.Printf("Book: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	bookCandidate := scanner.Text()
+	for _, v := range orderedBooks {
+		if strings.ToUpper(bookCandidate) == v {
+			m.Book = strings.ToLower(v)
+			return
+		}
+	}
+	log.Fatalf("book: %s does not exist, these are available: %s",
+		bookCandidate, orderedBooks)
+}
+
+func (m *Message) setChapter() {
+	log.Println("running setChapter")
+	// Taking input from user
+	fmt.Printf("Chapter: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	chapterCandidate := scanner.Text()
+	if chapterCandidate == "" {
+		m.Chapter = 0
+		return
+	}
+	t, err := strconv.Atoi(chapterCandidate)
+	if err != nil {
+		log.Fatalf("Failed to set chapter: %s", chapterCandidate)
+	}
+	m.Chapter = t
+}
+
+func (m *Message) setVerseStart() {
+	log.Println("running setVerseStart")
+	// Taking input from user
+	fmt.Println("Verse Start: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	verseStartCandidate := scanner.Text()
+	if verseStartCandidate == "" {
+		m.VerseStart = 0
+		return
+	}
+	t, err := strconv.Atoi(verseStartCandidate)
+	if err != nil {
+		log.Fatalf("Failed to set verseStart: %s", verseStartCandidate)
+	}
+	m.VerseStart = t
+}
+
+func (m *Message) setVerseEnd() {
+	log.Println("running setVerseEnd")
+	// Taking input from user
+	fmt.Printf("VerseEnd: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	verseEndCandidate := scanner.Text()
+	if verseEndCandidate == "" {
+		m.VerseEnd = 0
+		return
+	}
+	t, err := strconv.Atoi(verseEndCandidate)
+	if err != nil {
+		log.Fatalf("Failed to set verseEnd: %s", verseEndCandidate)
+	}
+	m.VerseEnd = t
+}
+
+func (m *Message) setPart() {
+	log.Println("running setPart")
+	// Taking input from user
+	fmt.Println("Part number: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	PartCandidate := scanner.Text()
+	if PartCandidate == "" {
+		m.Part = 0
+		return
+	}
+	t, err := strconv.Atoi(PartCandidate)
+	if err != nil {
+		log.Fatalf("Failed to set Part: %s", PartCandidate)
+	}
+	m.Part = t
+}
+
+func (m *Message) writeIt() {
+	log.Printf("Writing %s\n", m.FileName+".json")
+	file, _ := json.MarshalIndent(m, "", "")
+	_ = ioutil.WriteFile(m.FileName+".json", file, 0644)
+}
+
 func main() {
 	existing := pullExisting()
 	m := Message{}
-	// m.setFileName(&existing)
-	// m.setMD5Sum()
-	// m.getSpeaker(&existing)
-	// m.setTopic(&existing)
-	_ = existing
+	m.setFileName(&existing)
+	m.setMD5Sum()
+	m.getSpeaker(&existing)
+	m.setTopic(&existing)
+
 	m.setYear()
 	m.setMonth()
 	m.setDay()
+	m.setBook()
+	m.setChapter()
+	m.setVerseStart()
+	m.setVerseEnd()
+	m.setPart()
+	m.writeIt()
 	fmt.Printf("final result: %#v\n", m)
 }
